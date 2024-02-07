29390cdd4426083dfb2df589296ede44ee2056d5
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2024-02-07 11:50:20 +0530

    CustomError With Status Code Added

--- src/main/java/com/prajwal/todo_app/service/TodoService.java
+++ src/main/java/com/prajwal/todo_app/service/TodoService.java
@@ -5,7 +5,6 @@ import com.prajwal.todo_app.repository.TodoRepository;
 import com.prajwal.todo_app.util.CustomException;
 import com.prajwal.todo_app.util.TaskStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
